1feb7c72b523ce8089ae953add09a05eac5d1a5d
Mitchell Monahan <soccermitchy@gmail.com>
2015-05-25 23:30:15 -0400

    Create view to show how many minutes a user was on

--- migrations.lua
+++ migrations.lua
@@ -1,6 +1,6 @@
 local schema = require'lapis.db.schema'
 local types  = schema.types
-
+local db = require'lapis.db'
 return {
 	[1] = function()
 		schema.create_table("track",{})
@@ -8,5 +8,13 @@ return {
 		schema.add_column("track","timestamp","bigint NOT NULL DEFAULT extract(epoch from now())::bigint")
 		schema.add_column("track","clientid","smallint NOT NULL DEFAULT 0")
 		schema.add_column("track","clientname","character varying(30) NOT NULL")
+	end,
+	[2] = function()
+		db.query([[
+			CREATE OR REPLACE VIEW "public"."view_track" AS SELECT DISTINCT ON (track.clientname) track.clientname,
+				count(track.clientname) AS count
+			   FROM track
+			  GROUP BY track.clientname
+		]])
 	end
 }
